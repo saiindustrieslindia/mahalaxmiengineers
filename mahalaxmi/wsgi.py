eacfa55e4cad52b537a3028b79aaeb5ac7a2a610
Sai Industries <saiindustrieslindia@gmail.com>
2022-05-04 09:58:57 +0530

    new

--- mahalaxmi/wsgi.py
+++ mahalaxmi/wsgi.py
@@ -1,11 +1,4 @@
-echo "# mahalaxmi" >> README.md"""
-WSGI config for mahalaxmi project.
 
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
-"""
 
 import os
 
